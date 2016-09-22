75a8e9478a8f7314621be6adb93f3d79005dfdf3
Sourabh Shirhatti <shirhatti@outlook.com>
2016-09-21 17:15:59 -0700

    Add test project

---
+++ test/AspNetCoreModule.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace AspNetCoreModule.Tests
+{
+    public class BasicTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
