c88ef77d151ac1ad72d8b789bd2a873c0d5eb577
Pan Wang <panwang@microsoft.com>
2017-11-17 18:23:21 -0800

    update loadassembly and build

--- src/CommonLib/requesthandler.cxx
+++ src/CommonLib/requesthandler.cxx
@@ -13,3 +13,26 @@ REQUEST_HANDLER::REQUEST_HANDLER(
 REQUEST_HANDLER::~REQUEST_HANDLER()
 {
 }
+
+VOID
+REQUEST_HANDLER::ReferenceRequestHandler(
+    VOID
+) const
+{
+    InterlockedIncrement(&m_cRefs);
+}
+
+
+VOID
+REQUEST_HANDLER::DereferenceRequestHandler(
+    VOID
+) const
+{
+    DBG_ASSERT(m_cRefs != 0);
+
+    LONG cRefs = 0;
+    if ((cRefs = InterlockedDecrement(&m_cRefs)) == 0)
+    {
+        delete this;
+    }
+}
