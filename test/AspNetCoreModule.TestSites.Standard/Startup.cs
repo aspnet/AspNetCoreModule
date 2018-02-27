7cb7f3312b1bafc1780192292f5eadb0078549e2
Jeong Hwan Kim <jhkim@windows.microsoft.com>
2018-02-27 10:34:47 -0800

    Fix websocket bug

--- test/AspNetCoreModule.TestSites.Standard/Startup.cs
+++ test/AspNetCoreModule.TestSites.Standard/Startup.cs
@@ -140,7 +140,7 @@ namespace AspnetCoreModule.TestSites.Standard
                 {
                     if (context.WebSockets.IsWebSocketRequest)
                     {
-                        var webSocket = await context.WebSockets.AcceptWebSocketAsync("");
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
                         await Echo(webSocket);
                     }
                     else
